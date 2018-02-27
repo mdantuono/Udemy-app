ec1fe5cd20bc0d1dfde42e342d202cccafc97597
Michael DAntuono <mikeishere3@gmail.com>
2018-02-27 18:31:20 +0000

    udemy started

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+
+app.set('port', 3000);
+
+app.get('/', function(req, res){
+   console.log("GET the homepage") 
+});
+
+var server = app.listen(app.get('port'), function(){
+    var port = server.address().port;
+    console.log('Magic happens on port ' + port);
+});
+
+console.log("WHOA");
