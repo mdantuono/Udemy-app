ef8ec1fb4bb2f15d1d60c6f1bb0ab888f55f7394
Michael DAntuono <mikeishere3@gmail.com>
2018-03-06 17:38:07 +0000

    controller file made

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-var routes = require('./routes');
+var routes = require('./api/routes');
 
 app.set('port', process.env.PORT);
 
@@ -16,22 +16,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api', routes);
 
-app.get('/json', function(req, res){
-   console.log("GET the json");
-   res
-    .status(200)
-    .json({ "jsonData" : true });
-});
-
-app.get('/file', function(req, res){
-   console.log("GET the file");
-   res
-    .status(200)
-    .sendFile(path.join(__dirname, 'app.js'));
-});
 var server = app.listen(app.get('port'), function(){
     var port = server.address().port;
     console.log('Magic happens on port ' + port);
 });
-
-console.log("WHOA");
