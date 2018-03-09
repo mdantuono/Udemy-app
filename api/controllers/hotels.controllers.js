09e6237de6faafeb2cfa75b24e7e7f165535299a
Michael DAntuono <mikeishere3@gmail.com>
2018-03-09 16:05:02 +0000

    section1 branch made

--- api/controllers/hotels.controllers.js
+++ api/controllers/hotels.controllers.js
@@ -4,3 +4,9 @@ module.exports.hotelsGetAll = function(req, res) {
         .status(200)
         .json({ "jsonData" : true });
 };
+module.exports.hotelsGetOne = function(req, res) {
+    console.log("GET the hotels");
+       res
+        .status(200)
+        .json( hotelData );
+};
