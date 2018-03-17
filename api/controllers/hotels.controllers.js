3a0e7cecc1250d4fbd8c3abd15ff33bcad0ea625
Michael DAntuono <mikeishere3@gmail.com>
2018-03-17 17:09:02 +0000

    controllers working, getting hotel data

--- api/controllers/hotels.controllers.js
+++ api/controllers/hotels.controllers.js
@@ -1,12 +1,17 @@
+var hotelData = require('../data/hotel-data.json');
+
 module.exports.hotelsGetAll = function(req, res) {
-    console.log("GET the json");
-       res
-        .status(200)
-        .json({ "jsonData" : true });
-};
-module.exports.hotelsGetOne = function(req, res) {
     console.log("GET the hotels");
        res
         .status(200)
         .json( hotelData );
+};
+
+module.exports.hotelsGetOne = function(req, res) {
+    var hotelId = req.params.hotelId;
+    var thisHotel = hotelData[hotelId];
+    console.log("GET the hotels");
+       res
+        .status(200)
+        .json( thisHotel );
 };
