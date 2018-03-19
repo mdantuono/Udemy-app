f6d511b7721765ca9f3122828abff13e009244ee
Michael DAntuono <mikeishere3@gmail.com>
2018-03-19 22:27:52 +0000

    level 2 started

--- api/routes/index.js
+++ api/routes/index.js
@@ -6,8 +6,13 @@ var ctrlHotels = require('../controllers/hotels.controllers.js');
 router
     .route('/hotels')
     .get(ctrlHotels.hotelsGetAll);
+    
 router 
     .route('/hotels/:hotelId')
     .get(ctrlHotels.hotelsGetOne);
+    
+router
+    .route('/hotels/new')
+    .post(ctrlHotels.hotelsAddOne)
 
 module.exports = router;
