d4d3a821776eaee9bd004d16ee881544a91b6b31
Michael DAntuono <mikeishere3@gmail.com>
2018-03-26 18:00:18 +0000

    mongo database made

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('./api/data/dbconnection.js').open();
 var express = require('express');
 var app = express();
 var path = require('path');
@@ -15,7 +16,7 @@ app.use(function(req, res, next) {
 //Serve the index.html page using express static function
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(bodyParser.urlencoded({ extended : false }))
+app.use(bodyParser.urlencoded({ extended : false }));
 
 app.use('/api', routes);
 
